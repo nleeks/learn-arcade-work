c6a1b7b02c575d29c1fd6774dbe1f21100fdc442
unknown <nleeks@overlake.org>
2021-02-01 16:12:42 -0800

    final touches on lab 1

--- Lab 01 - First Program/git challenge.py
+++ Lab 01 - First Program/git challenge.py
@@ -2,4 +2,4 @@ print("""This weekend I'll sleep as much as possible.
 After that I might do some homework.""")
 print("I'll probably end up working on learning github.")
 print("I definitely need more practice with that.")
-print("For now, though, \nat least I can print on multiple lines in three different ways")
+print("For now, though, \n at least I can print on multiple lines in three different ways!")
